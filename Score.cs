250b776b07286d5b6e5868e07e5741650004ba01
GalaxyBlitz <akbaksh02@hotmail.com>
2017-09-04 18:59:03 +0100

    Create Score.cs

---
+++ Score.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; 
+
+public class Score : MonoBehaviour
+{
+
+    private float score = 0.0f;
+
+    private int difficultyLevel = 1;
+    private int maxDifficultyLevel = 10;
+    private int scoreToNextLevel = 10;
+
+    private bool isDead = false; 
+
+    public Text scoreText;
+
+    public DeathMenu deathmenu; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (isDead)
+            return; 
+
+        if (score >= scoreToNextLevel)
+            LevelUp(); 
+
+        score += Time.deltaTime; 
+        scoreText.text = ((int)score).ToString(); 
+
+	}
+
+    void LevelUp()
+    {
+        if (difficultyLevel == maxDifficultyLevel)
+            return; 
+    
+        scoreToNextLevel *= 2;
+
+        difficultyLevel++;
+
+        //change <> to player motor or ball in future 
+        GetComponent<PlayerMotor>().SetSpeed(difficultyLevel);
+
+
+    }
+
+    public void OnDeath()
+    {
+        isDead = true;
+
+        if(PlayerPrefs.GetFloat("Highscore") < score)
+            PlayerPrefs.SetFloat("Highscore", score); 
+
+        deathmenu.ToggleEndMenu (score); 
+       
+    }
+
+}
